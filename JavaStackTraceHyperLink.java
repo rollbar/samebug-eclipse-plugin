5252e4c5458ed5f48d5b4da784291e5f9182c54a
vesakr <vessa.kr@gmail.com>
2017-07-11 11:48:14 +0200

    Add files via upload

---
+++ JavaStackTraceHyperLink.java
@@ -0,0 +1,50 @@
+package samebug.plugin.test.handlers;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.ui.console.IHyperlink;
+
+public class JavaStackTraceHyperLink implements IHyperlink {
+
+	private String exceptionName;
+
+	public JavaStackTraceHyperLink(String ename) {
+		this.exceptionName = ename;
+	}
+
+	@Override
+	public void linkActivated() {
+		Desktop d = Desktop.getDesktop();
+		try {
+			d.browse(new URI("http://google.com"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void linkEntered() {
+
+	}
+
+	@Override
+	public void linkExited() {
+
+	}
+
+	public String getExceptionName() {
+		return exceptionName;
+	}
+
+	public void setExceptionName(String exceptionName) {
+		this.exceptionName = exceptionName;
+	}
+
+}
